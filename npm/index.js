3dd342f13c2a3e0085a2aeb0bec21ac2bfa875a1
Radi Cho <radicho123@gmail.com>
2018-08-13 13:43:58 +0300

    Fix #1. patch 1.0.9

--- npm/index.js
+++ npm/index.js
@@ -1,9 +1,14 @@
-function findIntersection([P1, P2, P3, P4]) {
-  const x =
+function findIntersection(array) {
+  var P1 = array[0],
+    P2 = array[1],
+    P3 = array[2],
+    P4 = array[3];
+
+  var x =
     ((P1.x * P2.y - P2.x * P1.y) * (P3.x - P4.x) -
       (P1.x - P2.x) * (P3.x * P4.y - P3.y * P4.x)) /
     ((P1.x - P2.x) * (P3.y - P4.y) - (P1.y - P2.y) * (P3.x - P4.x));
-  const y =
+  var y =
     ((P1.x * P2.y - P2.x * P1.y) * (P3.y - P4.y) -
       (P1.y - P2.y) * (P3.x * P4.y - P3.y * P4.x)) /
     ((P1.x - P2.x) * (P3.y - P4.y) - (P1.y - P2.y) * (P3.x - P4.x));
@@ -11,19 +16,29 @@ function findIntersection([P1, P2, P3, P4]) {
 }
 
 function isPointBetween(p, a, b) {
-  return ((a.x <= p.x && p.x <= b.x) || (a.x >= p.x && p.x >= b.x)) && ((a.y <= p.y && p.y <= b.y) || (a.y >= p.y && p.y >= b.y));
+  return (
+    ((a.x <= p.x && p.x <= b.x) || (a.x >= p.x && p.x >= b.x)) &&
+    ((a.y <= p.y && p.y <= b.y) || (a.y >= p.y && p.y >= b.y))
+  );
 }
 
 function findSegmentIntersection(points) {
-  const i1 = findIntersection(points);
-  const [P1, P2, P3, P4] = points;
-  const isIntersected = isPointBetween(i1, P1, P2) && isPointBetween(i1, P3, P4);
+  var i1 = findIntersection(points);
+  var P1 = points[0],
+    P2 = points[1],
+    P3 = points[2],
+    P4 = points[3];
+  const isIntersected =
+    isPointBetween(i1, P1, P2) && isPointBetween(i1, P3, P4);
   return isIntersected ? i1 : false;
 }
 
 function isSegmentIntersected(points) {
-  const i1 = findIntersection(points);
-  const [P1, P2, P3, P4] = points;
+  var i1 = findIntersection(points);
+  var P1 = points[0],
+    P2 = points[1],
+    P3 = points[2],
+    P4 = points[3];
   return isPointBetween(i1, P1, P2) && isPointBetween(i1, P3, P4);
 }
 
